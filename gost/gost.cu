b6a901bff7561eb08e1334b0753151bf7687e2e2
orignal <i2porignal@yandex.ru>
2017-04-17 16:26:26 -0400

    call streebog_cpu_hash_64

--- gost/gost.cu
+++ gost/gost.cu
@@ -32,6 +32,8 @@ extern "C" void gosthash(void *output, const void *input)
 	memcpy(output, hash, 32);
 }
 
+extern void streebog_cpu_hash_64(int thr_id, uint32_t threads, uint32_t *d_hash);
+
 //#define _DEBUG
 #define _DEBUG_PREFIX "sib"
 #include "cuda_debug.cuh"
@@ -78,8 +80,8 @@ extern "C" int scanhash_gost(int thr_id, struct work* work, uint32_t max_nonce, 
 		int order = 0;
 
 		// Hash with CUDA
-		//streebog_cpu_hash_64(thr_id, throughput, d_hash[thr_id]);
-		//TRACE("gost   :");
+		streebog_cpu_hash_64(thr_id, throughput, d_hash[thr_id]);
+		TRACE("gost   :");
 
 		work->nonces[0] = cuda_check_hash(thr_id, throughput, pdata[19], d_hash[thr_id]);
 		if (work->nonces[0] != UINT32_MAX)
